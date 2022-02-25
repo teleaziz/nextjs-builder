41890e1d364890e4c10055749a1b7875773c47e1
Aziz Abbas <aziz@builder.io>
2022-02-25 13:05:42 -0800

    try this

--- pages/[[...page]].tsx
+++ pages/[[...page]].tsx
@@ -5,7 +5,10 @@ import DefaultErrorPage from 'next/error'
 import Head from 'next/head'
 import builderConfig from '@config/builder'
 import '@builder.io/widgets'
-require('vm2');
+
+if (Builder.isServer) {
+  require('vm2');
+}
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
 
